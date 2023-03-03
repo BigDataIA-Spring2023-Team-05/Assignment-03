e53dac6c84c0205439a7d74ccc54bf543b0a3567
krishicagopal <gopalakrishnan.kr@northeastern.edu>
2023-03-02 21:40:19 -0500

    Registering new users to choose plans

--- frontend/Login.py
+++ frontend/Login.py
@@ -70,7 +70,7 @@ else:
 # if st.session_state["authentication_status"]:
 #     authenticator.logout('Logout', 'main')
 #     st.write(f'Welcome *{st.session_state["name"]}*')
-#     st.title('Some content')
+#     st.title('Some content')    
 # elif st.session_state["authentication_status"] == False:
 #     st.error('Username/password is incorrect')
 # elif st.session_state["authentication_status"] == None:
