0133758280abafa3b497d04ac11b26b6b3a50d92
“Charan-Patnaik” <avscharan.patnaik@gmail.com>
2023-03-03 18:17:14 -0500

    changes to pages

--- frontend/pages/check.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-import numpy as np
-dataframe = pd.DataFrame({'Calls': np.random.randint(0,4,24), 'Time of the Day': pd.date_range('00:00', '23:00', freq='H')})
-print(dataframe)
