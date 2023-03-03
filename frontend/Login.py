bde4cc76d116df0b77c10779a6d7a50fb7c6b554
“Charan-Patnaik” <avscharan.patnaik@gmail.com>
2023-03-03 04:28:16 -0500

    Login and Forgot UI configured

--- frontend/Login.py
+++ frontend/Login.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-
+from streamlit_extras.switch_page_button import switch_page
 
 
 ## 
@@ -13,7 +13,40 @@ def fetch(session, url):
 
 # Create an empty container
 placeholder = st.empty()
+placeholder_2 = st.empty()
+placeholder_3 = st.empty()
+placeholder_4 = st.empty()
+placeholder_5 = st.empty()
+# submit = False
+# submit_2 = False
+# submit_3 = False
+# submit_4 = False
+# submit_5 = False
+# if 'submit_2' not in st.session_state:
+#     st.session_state['submit_2'] = False
+# if 'submit_3' not in st.session_state:
+#     st.session_state['submit_3'] = False
+# if 'submit_4' not in st.session_state:
+#     st.session_state['submit_4'] = False
+# if 'submit_5' not in st.session_state:
+#     st.session_state['submit_5'] = False
+# if st.session_state.get('submit_5') != True:
 
+#     st.session_state['submit_2'] = submit_2
+
+# if st.session_state.get('submit_3') != True:
+#     st.session_state['submit_3'] = submit_3
+
+# if st.session_state.get('submit_4') != True:
+#     st.session_state['submit_4'] = submit_4
+
+# if st.session_state.get('submit_5') != True:
+#     st.session_state['submit_5'] = submit_5
+# var = False
+# var_2 = False
+# var_3 = False
+# var_4 = False
+# var_5 = False
 actual_email = "admin"
 actual_password = "admin"
 session = requests.Session()
@@ -21,12 +54,71 @@ session = requests.Session()
 # Insert a form in the container
 if 'authentication_status' not in st.session_state:
     st.session_state['authentication_status'] = False
+if 'forgot_status' not in st.session_state:
+    st.session_state['forgot_status'] = False
 if st.session_state["authentication_status"] == False:
     with placeholder.form("login"):
+        var = True
+        # var_2 = True
+        # var_3 = True
+        # var_4 = True
+        # var_5 = True
         st.markdown("#### Enter your credentials")
         username = st.text_input("UserName")
         password = st.text_input("Password", type="password")
-        submit = st.form_submit_button("Login")
+        column_1,column_2,column_3 = st.columns([1,6,1.75])
+        with column_1:
+            submit = st.form_submit_button("Login")
+            
+        with column_2:
+            st.write = " "
+        with column_3:
+            submit_2 = st.form_submit_button("Forgot Password")
+            
+    # if st.button    
+            if submit_2:
+                st.session_state['forgot_status'] = True
+                switch_page('Forgot_password')
+
+
+    # if st.session_state['submit_2']: 
+    #     placeholder_2.empty()
+    #     # var = True
+    #     # var_2 = True
+    #     # var_3 = True
+    #     # var_4 = True
+    #     # var_5 = True
+    #     with placeholder_2.form("forgot"):    
+    #         # placeholder.empty()
+    #         # with placeholder.form("Forgot_Password"):
+    #         f_email = st.text_input("Please enter your email")
+    #         submit_3 = st.form_submit_button("Send OTP")
+    #         print("submit_3  "+str(submit_3))
+    #         if submit_3:
+
+    #             st.session_state['submit_3'] = True
+    #             # var_3 = True
+    #             # var_2 = True
+
+    # if st.session_state['submit_3']:
+    #     with placeholder_3.form("otp"): 
+    #         print("send otp button working")
+    #         otp = st.text_input("Enter verfication OTP")
+    #         submit_4 = st.form_submit_button("Verify")
+    #         if submit_4:
+    #             st.session_state['submit_4'] = True
+    # if st.session_state['submit_4']:
+    #     with placeholder_4.form("password"): 
+    #         password = st.text_input("Password", type="password")
+    #         confirm_password = st.text_input("Confirm Password", type="password")
+    #         submit_5 = st.form_submit_button("Done")
+    #         if submit_5:
+    #           st.session_state.done = True  
+    # if st.session_state.done:
+    #     st.success("Password changed") 
+
+
+
     if submit:
         # If the form is submitted and the email and password are correct,
         # clear the form/container and display a success message
